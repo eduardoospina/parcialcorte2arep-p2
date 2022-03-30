3897fb763a2d46c086ae4c2ee73e910960c6875c
eduardoospina <eduardo.ospina@mail.escuelaing.edu.co>
2022-03-30 16:46:05 -0500

    subir final

--- src/main/java/edu/escuelaing/arep/MathServices.java
+++ src/main/java/edu/escuelaing/arep/MathServices.java
@@ -5,13 +5,16 @@ import org.json.simple.JSONObject;
 
 public class MathServices {
 
-    private static Mathimplementaction Mathimple = new Mathimplementaction();
-
     public static void main(String[] args) {
-        JSONObject jsonObject = new JSONObject();
         port(getPort());
 
         get("/hello", (req, res) -> "Hello Heroku");
+
+        get("/sqrt", ((req, res) -> {
+            res.type("application/json");
+            Double input = Double.parseDouble(req.queryParams("value"));
+            return Mathimplementaction.Sqrt(input);
+        }));
 
         get("/atan", ((req, res) -> {
             res.type("application/json");
@@ -19,17 +22,12 @@ public class MathServices {
             return Mathimplementaction.Atan(input);
         }));
 
-        get("/sqrt", ((req, res) -> {
-            res.type("application/json");
-            Double input = Double.parseDouble(req.queryParams("value"));
-            return Mathimplementaction.Sqrt(input);
-        }));
     }
 
     static int getPort() {
         if (System.getenv("PORT") != null) {
             return Integer.parseInt(System.getenv("PORT"));
         }
-        return 34001; //returns default port if heroku-port isn't set(i.e. on localhost)
+        return 34002; //returns default port if heroku-port isn't set(i.e. on localhost)
     }
 }
--- src/main/java/edu/escuelaing/arep/Mathimplementaction.java
+++ src/main/java/edu/escuelaing/arep/Mathimplementaction.java
@@ -6,19 +6,27 @@ import java.lang.Math;
 public class Mathimplementaction {
 
 
-    private static JSONObject makeJson(String operation, Double input, Double output){
+    private static JSONObject makeJson1(String operation, Double input, Double output){
         JSONObject json = new JSONObject();
         json.put("operation", operation);
         json.put("input", input);
-        json.put("output", output);
+        json.put("output", Math.atan(output));
+        return json;
+    }
+
+    private static JSONObject makeJson2(String operation, Double input, Double output){
+        JSONObject json = new JSONObject();
+        json.put("operation", operation);
+        json.put("input", input);
+        json.put("output", Math.sqrt(output));
         return json;
     }
 
     public static JSONObject Atan(Double input){
-        return makeJson("atan", input, Math.atan(input));
+        return makeJson1("atan", input, input);
     }
 
     public static JSONObject Sqrt(Double input){
-        return makeJson("sqrt", input, Math.sqrt(input));
+        return makeJson2("sqrt", input, input);
     }
 }
